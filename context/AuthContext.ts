88ad67743ce4c500e4b6076bf142ac059be34bd0
Mohamed-Madani <m.rabiouhalilou@gmail.com>
2024-09-04 08:18:21 +0000

    Creating the Authentification with firebase

---
+++ context/AuthContext.ts
@@ -0,0 +1,88 @@
+'use client'
+
+import { createContext, useContext, useEffect, useState } from 'react'
+import { auth, db } from '@/firebase'
+import { doc, getDoc } from 'firebase/firestore'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth'
+
+
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+    const [currentUser, setCurrentUser] = useState(null)
+    const [userDataObj, setUserDataObj] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    //Auth Handlers
+    function signup(email: string, password: string) {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    function login(email: string, password: string) {
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    function logout() {
+        setUserDataObj({})
+        setCurrentUser(null)
+        return signOut(auth)
+    }
+
+    useEffect(() => {
+
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            try {
+
+                //set to user to our local context state
+                setLoading(true)
+                setCurrentUser(user)
+
+                if (!user) {
+                    return
+                }
+
+                //if user exists, fetch data from firestore database
+                console.log('Fetching User Data')
+                const docRef = doc(db, 'users', user.uid)
+                const docSnap = await getDoc(docRef)
+                let firebaseData = {}
+                if (docSnap.exists()) {
+                    console.log('Found User Data')
+                    firebaseData = docSnap.data()
+                    console.log(firebaseData)
+                }
+                setUserDataObj(firebaseData)
+
+
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setLoading(false)
+            }
+        })
+
+    }, [])
+
+    const value = {
+        currentUser,
+        userDataObj,
+        signup,
+        logout,
+        login,
+        loading,
+
+    }
+
+    
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+}
