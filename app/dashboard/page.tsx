f39219a52e05a61edbdbb53bdc3339a990a5c549
Mohamed-Madani <m.rabiouhalilou@gmail.com>
2024-09-04 20:31:28 +0000

    Creating the loading component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,17 +2,22 @@ import Dashboard from "@/components/Dashboard";
 import Login from "@/components/Login";
 import Main from "@/components/Main";
 import type { Metadata } from "next";
+import { useAuth } from "@/context/AuthContext";
 
 export const metadata: Metadata = {
   title: "Moodly • Dashboard",
 };
 
 export default function DashboardPage() {
-  const isAuthenticated = true;
+  const {currentUser, loading} = useAuth()
 
   let children = <Login />;
 
-  if (isAuthenticated) {
+  if (loading) {
+    
+  }
+
+  if (currentUser) {
     children = <Dashboard />;
   }
   return <Main className="">
